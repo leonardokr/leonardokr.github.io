1d361f9c341a85bfa536df21a26e5f98817c8cff
Leonardo K. R. <leo@ziondev.us>
2025-08-27 16:53:51 -0300

    feat: update SVG trace color in image queries to improve visual consistency

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,7 +51,7 @@ export const pageQuery = graphql`
             title
             avatar {
               childImageSharp {
-                fluid(maxWidth: 700, quality: 90, traceSVG: { color: "#64ffda" }) {
+                fluid(maxWidth: 700, quality: 90, traceSVG: { color: "#00c1e1" }) {
                   ...GatsbyImageSharpFluid_withWebp_tracedSVG
                 }
               }
@@ -89,7 +89,7 @@ export const pageQuery = graphql`
             title
             cover {
               childImageSharp {
-                fluid(maxWidth: 700, quality: 90, traceSVG: { color: "#64ffda" }) {
+                fluid(maxWidth: 700, quality: 90, traceSVG: { color: "#00c1e1" }) {
                   ...GatsbyImageSharpFluid_withWebp_tracedSVG
                 }
               }
